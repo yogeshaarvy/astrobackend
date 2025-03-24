500a498788231a79b8a31b6213aba23cb3793d02
Vikash Singhal <vikash.singhal@aarvytechnologies.co.in>
2025-03-24 17:44:48 +0530

    fix code

--- app/dashboard/filters/values/[valueId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import ValuesViewPage from '../_components/value-view-page';
-
-export const metadata = {
-  title: 'Dashboard : Values View'
-};
-
-export default function Page() {
-  return <ValuesViewPage />;
-}
--- redux/store.ts
+++ redux/store.ts
@@ -45,7 +45,7 @@ import blogsReducer from './slices/pages/bloges/categorySlice';
 import promiseReducer from './slices/pages/promiseSlice';
 import storeFaq from './slices/store/faqSlice';
 import configReducer from './slices/home/configSlice';
-import storeConfigReducer from './slices/store/storeconfigSlice';
+import storeConfigReducer from './slices/store/storeconfigSliceTest';
 import storegallery from './slices/store/gallerySlice';
 import filterReducer from './slices/store/filtersSlice';
 
